b6b83b416aa36d29f6446f736f22e6f016a9cf4f
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-11-15 17:58:33 -0300

    Fix: teams model

---
+++ app/backend/src/database/models/Teams.model.ts
@@ -0,0 +1,36 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model<InferAttributes<Teams>,
+InferCreationAttributes<Teams>> {
+  declare id: CreationOptional<number>;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Teams;
