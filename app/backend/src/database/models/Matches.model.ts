764cb3eb9aa79c42035adbfc25021bdc5b014b9b
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2023-11-16 20:22:06 -0300

    Feat: req 13, lint checked

---
+++ app/backend/src/database/models/Matches.model.ts
@@ -0,0 +1,55 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Matches extends Model<InferAttributes<Matches>,
+InferCreationAttributes<Matches>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+Matches.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+export default Matches;
